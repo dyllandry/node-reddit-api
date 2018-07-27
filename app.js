dbbeb2a57f9f00d8d27e7f7b41a48993c4cedcc6
Dylan Landry <dylanglandry@gmail.com>
2018-07-27 18:02:42 -0400

    pug templating engine

--- app.js
+++ app.js
@@ -2,32 +2,34 @@ const RedditApi = require('./reddit_api');
 // Routing and middleware package
 const Express = require('express');
 const App = Express();
+// Node's path module for constructing file paths appropriate for system.
+const Path = require('path');
+
+// Setting Express view engine to pug package, they integrate with eachother.
+App.set('view engine', 'pug');
+// Set view directory.
+App.set('views', Path.join(__dirname, 'browser/views'));
 
 // Set '/' route for GET HTTP requests w/ express. Lookup routing if confused.
 App.get('/', (request, response) => {
     // UPGRADE: Log in a log.txt file instead.
-    console.log('Serving GET request on "/"');
+    console.log('\nServing GET request on "/"');
 
+    // JavaScript promises, they're confusing at first.
     RedditApi.Authenticate().then(token => {
         RedditApi.GetHot(token).then(posts  => {
             
-            let responseBody = '<ul>';
-            posts.forEach(post => {
-                responseBody += '<a href="' + post.data.url + '">'
-                    + '<li>' + post.data.title + '</li>'
-                    + '</a>';
-            })        
-            responseBody += '</ul>';
-            response.send(responseBody);
+            // Display browser/views/index.pug as html and insert posts data. 
+            response.render('index', {posts: posts});
 
-        }).catch(logError)
+        }).catch(logError);
     }).catch(logError);  
-})
+});
 
 // Start listening for requests from localhost:3000
 App.listen('3000', () => {
-    console.log('Express listening on port 3000');
-})
+    console.log('Express listening on port 3000...');
+});
 
 function logError(error) {
     console.log(error);

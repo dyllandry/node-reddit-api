c178d8dfb2ea7626d70d482006d24fa9bfd802c2
Dylan Landry <dylanglandry@gmail.com>
2018-07-27 17:05:45 -0400

    Posts served on /

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const RedditApi = require('./reddit_api');
+// Routing and middleware package
+const Express = require('express');
+const App = Express();
 
-RedditApi.Authenticate.then((token) => {
-    RedditApi.GetHot(token).then(posts  => {
-        posts.forEach(post => {
-            console.log(post.data.title);
-        })        
-    }).catch(logError)
-}).catch(logError);
+// Set '/' route for GET HTTP requests w/ express. Lookup routing if confused.
+App.get('/', (request, response) => {
+    // UPGRADE: Log in a log.txt file instead.
+    console.log('Serving GET request on "/"');
+
+    RedditApi.Authenticate().then(token => {
+        RedditApi.GetHot(token).then(posts  => {
+            
+            let responseBody = '<ul>';
+            posts.forEach(post => {
+                responseBody += '<a href="' + post.data.url + '">'
+                    + '<li>' + post.data.title + '</li>'
+                    + '</a>';
+            })        
+            responseBody += '</ul>';
+            response.send(responseBody);
+
+        }).catch(logError)
+    }).catch(logError);  
+})
+
+// Start listening for requests from localhost:3000
+App.listen('3000', () => {
+    console.log('Express listening on port 3000');
+})
 
 function logError(error) {
     console.log(error);
--- reddit_api/authentication.js
+++ reddit_api/authentication.js
@@ -3,57 +3,59 @@ const Https = require('https');
 // NPM package 'data-store' used to store data.
 const Store = require('data-store');
 
-// There are many good JavaScript promise guides if this is confusing.
-module.exports = new Promise(function(resolve, reject) {
-    // Creates config.json if it doesn't exist.
-    const store = new Store('config', {base: __dirname});
-
-    // The Reddit API requires passing a random 30 character ASCII device id.
-    if (!store.hasOwn('deviceId') || !store.has('deviceId')) {
-        const deviceId = _generateAlphaNumeric(30);
-        store.set('deviceId', deviceId);
-    }
-
-    // UPGRADE: Only request a new token if the previous expired.
-    // Https post options for our token request
-    const options = {
-        hostname: 'www.reddit.com',
-        path: '/api/v1/access_token',
-        method: 'POST',
-        headers: {
-            'Authorization' : 'Basic ' + new Buffer('pU2nZ9uJxAyCbw:').toString('base64'),
+function Authenticate() {
+    // There are many good JavaScript promise guides if this is confusing.
+    return new Promise(function(resolve, reject) {
+        // Creates config.json if it doesn't exist.
+        const store = new Store('config', {base: __dirname});
+    
+        // The Reddit API requires passing a random 30 character ASCII device id.
+        if (!store.hasOwn('deviceId') || !store.has('deviceId')) {
+            const deviceId = _generateAlphaNumeric(30);
+            store.set('deviceId', deviceId);
         }
-    };
-
-    const request = Https.request(options, (response) => {
-        // Build the response we receive piece by piece.
-        let responseBody = '';
-        response.on('data', (d) => {
-            responseBody += d;
+    
+        // UPGRADE: Only request a new token if the previous expired.
+        // Https post options for our token request
+        const options = {
+            hostname: 'www.reddit.com',
+            path: '/api/v1/access_token',
+            method: 'POST',
+            headers: {
+                'Authorization' : 'Basic ' + new Buffer('pU2nZ9uJxAyCbw:').toString('base64'),
+            }
+        };
+    
+        const request = Https.request(options, (response) => {
+            // Build the response we receive piece by piece.
+            let responseBody = '';
+            response.on('data', (d) => {
+                responseBody += d;
+            });
+            
+            // Convert completed response string to json on 'end' event.
+            response.on('end', () => {
+                const jsonBody = JSON.parse(responseBody);
+                console.log("Token retrieved");
+                resolve(jsonBody.access_token);
+            });
         });
-        
-        // Convert completed response string to json on 'end' event.
-        response.on('end', () => {
-            const jsonBody = JSON.parse(responseBody);
-            console.log("Token retrieved");
-            resolve(jsonBody.access_token);
+    
+        request.on('error', (error) => {
+            reject(error);
         });
+    
+        /* How we write data into our POST request.
+        What data to include is detailed by the Reddit OAuth2 guide.
+        https://github.com/reddit-archive/reddit/wiki/OAuth2#application-only-oauth
+        */
+        let postData = '';
+        postData += 'grant_type=https://oauth.reddit.com/grants/installed_client';
+        postData += '&device_id=' + store.get('deviceId');
+        request.write(postData)
+        request.end();
     });
-
-    request.on('error', (error) => {
-        reject(error);
-    });
-
-    /* How we write data into our POST request.
-    What data to include is detailed by the Reddit OAuth2 guide.
-    https://github.com/reddit-archive/reddit/wiki/OAuth2#application-only-oauth
-    */
-    let postData = '';
-    postData += 'grant_type=https://oauth.reddit.com/grants/installed_client';
-    postData += '&device_id=' + store.get('deviceId');
-    request.write(postData)
-    request.end();
-});
+}
 
 // Functions meant to be used only by the internal module are prefixed with '_'
 function _generateAlphaNumeric(length) {
@@ -64,3 +66,5 @@ function _generateAlphaNumeric(length) {
     }
     return string;
 }
+
+module.exports = Authenticate;
